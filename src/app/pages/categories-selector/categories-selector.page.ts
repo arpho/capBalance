c30b1a44e740397efa7bebc03f1aa74cba660456
giuseppe D'Amico <damicogiuseppe77@gmail.com>
2019-07-19 11:33:05 +0200

    categoriesSelector
    le categorie selezionate sono rimosse dalla lista delle categorie selezionabili

--- src/app/pages/categories-selector/categories-selector.page.ts
+++ src/app/pages/categories-selector/categories-selector.page.ts
@@ -17,13 +17,18 @@ export class CategoriesSelectorPage implements OnInit {
   selectedCategoryIcon = 'remove'
   colorSelectableCategory = 'green' // add category green 
   colorSelectedCategory = 'orange'
+  filterFunction: (item: CategoryModel) => boolean
   sorterFunction = (a: ItemModelInterface, b: ItemModelInterface) => (a.title < b.title ? -1 : (a.title > b.title ? 1 : 0))
-  filterFunction = (a: ItemModelInterface) => this.selectedCategoriesList.map((cat: ItemModelInterface) => cat.key).includes(a.key)
+
 
   constructor(public modalCtrl: ModalController, public categories: CategoriesService) { }
+  filterFactory(args: { selectedCategoriesList: Array<CategoryModel> }) {
+    return (a: ItemModelInterface) => !args.selectedCategoriesList.map((cat: ItemModelInterface) => cat.key).includes(a.key)
+  }
 
   ngOnInit() {
     this.selectedCategoriesList = []
+    this.filterFunction = this.filterFactory({ selectedCategoriesList: this.selectedCategoriesList })
     if (this.categories.getEntitiesList()) {
       this.categories.getEntitiesList().on('value', snap => {
         this.categoriesList = []
@@ -37,12 +42,11 @@ export class CategoriesSelectorPage implements OnInit {
   removeCategory(category) {
     this.selectedCategoriesList = this.selectedCategoriesList.filter((item: CategoryModel) => item.key !== category.key)
     this.filterFunction = (a: ItemModelInterface) => this.selectedCategoriesList.map((item: ItemModelInterface) => item.key).includes(a.key)
+    this.filterFunction = this.filterFactory({ selectedCategoriesList: this.selectedCategoriesList })
   }
   addCategory(cat) {
     this.selectedCategoriesList = [...this.selectedCategoriesList, cat]
-    this.filterFunction = (a: ItemModelInterface) => {
-      return this.selectedCategoriesList.map((item: ItemModelInterface) => item.key).includes(a.key)
-    }
+    this.filterFunction = this.filterFactory({ selectedCategoriesList: this.selectedCategoriesList })
 
   }
 
