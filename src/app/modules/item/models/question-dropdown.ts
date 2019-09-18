3682a93afb28a2134a9a69bfafe849efdf6f444d
giuseppe D'Amico <damicogiuseppe77@gmail.com>
2019-09-18 17:42:34 +0200

    Piechart-page
    fixed type's question

--- src/app/modules/item/models/question-dropdown.ts
+++ src/app/modules/item/models/question-dropdown.ts
@@ -1,10 +1,11 @@
 import { QuestionBase } from './question-base';
+import { DropdownProperties } from '../../dynamic-form/models/question-dropdown';
 
 export class DropdownQuestion extends QuestionBase<string> {
     controlType = 'dropdown';
     options: { key: string, value: string }[] = [];
 
-    constructor(options = { key: 'dropdown', label: 'combobox' }) {
+    constructor(options: { key: string, label: string } | DropdownProperties) {
         super(options);
         // tslint:disable-next-line: no-string-literal
         this.options = options['options'] || [];
