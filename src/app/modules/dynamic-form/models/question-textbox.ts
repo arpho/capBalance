2520a98c20d571a18616b8b9851fd4a45c718e8f
giuseppe D'Amico <damicogiuseppe77@gmail.com>
2019-09-18 17:26:59 +0200

    Categories'page
    fixed type's questions

--- src/app/modules/dynamic-form/models/question-textbox.ts
+++ src/app/modules/dynamic-form/models/question-textbox.ts
@@ -1,10 +1,11 @@
 import { QuestionBase } from './question-base';
+import { QuestionProperties } from './questionproperties';
 
 export class TextboxQuestion extends QuestionBase<string> {
     controlType = 'textbox';
     type: string;
 
-    constructor(options = { key: 'text', label: 'set the text' }) {
+    constructor(options : { key: string, label: string }|QuestionProperties<string>) {
         super(options);
         // tslint:disable-next-line: no-string-literal
         this.type = options['type'] || '';
